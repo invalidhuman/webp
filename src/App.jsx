a080bb093d0a3fcbbbd517756cd4be2ca820510d
seunggonhyeon <hyunsg5638@gmail.com>
2025-05-25 14:30:29 +0900

    feat:fetch 함수 경로를/api로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
-import { useState } from "react";
-export default function App() {
-  const [isDark, setIsDark] = useState(false);
-  const toggleMode = () => setIsDark(!isDark);
+import { useEffect, useState } from "react";
 
-  return (
-    <div
-      className={`min-h-screen flex flex-col items-center justify-center transition-colors duration-300 ${
-        isDark ? "bg-[#222] text-white" : "bg-[#fff] text-black"
-      }`}
-    >
-      <h1>{isDark ? "! Dark Mode" : "☀ Light Mode"}</h1>
-      <button
-        onClick={toggleMode}
-        className="mt-4 px-4 py-2x rounded bg-gray-200 border-2 border-gray-950 text-black"
-      >
-        {isDark ? "Light Mode로 전환" : "Dark Mode로 전환"}
-      </button>
-    </div>
+function App() {
+  const [loaded, setLoaded] = useState(false);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((response) => response.json())
+      .then((json) => {
+        setData(json.data);
+        setLoaded(true);
+      });
+  }, []);
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
+  return loaded ? (
+    <ol>
+      {data.map(({ id, title }) => (
+        <li key={id}>{title}</li>
+      ))}
+    </ol>
+  ) : (
+    "데이터 fetch 미완료"
   );
 }
+
+export default App;
